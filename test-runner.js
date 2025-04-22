3f6de6b5f87c5593d3f8aac10e0b0844b0eea55e
malikhaseebtahir <malikhaseeb1935@gmail.com>
2025-04-22 22:23:06 +0200

    Updated test-runner.js and fixed a minor bug

--- test-runner.js
+++ test-runner.js
@@ -1,10 +1,14 @@
 // test-runner.js
 const { execSync } = require("child_process");
 
-const fileArg = process.argv[2]; // get the filename if provided
+const fileArg = process.argv[2]; // Get file if passed
 
 const baseCmd = "jest --maxWorkers=50%";
-
 const cmd = fileArg ? `${baseCmd} ${fileArg}` : baseCmd;
 
-execSync(cmd, { stdio: "inherit" });
+try {
+  execSync(cmd, { stdio: "inherit" });
+} catch (err) {
+  // Allow Jest to print the error output without crashing the script
+  process.exit(1);
+}
